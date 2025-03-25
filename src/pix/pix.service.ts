244551b2f40a3dae5e5a3ed00fc44d0509882398
Matheus Braga de Carvalho <matheus.carvalho@globalsys.com.br>
2025-03-25 10:59:09 -0300

    Update-PIX

--- src/pix/pix.service.ts
+++ src/pix/pix.service.ts
@@ -1,13 +1,13 @@
 // src/pix/pix.service.ts
 import { Injectable } from '@nestjs/common';
-import { createDynamicPix, createStaticPix, hasError } from 'pix-utils';
+import { createStaticPix, hasError } from 'pix-utils';
 
 @Injectable()
 export class PixService {
   async gerarQrCode(chavePix: string, valor: number, descricao: string) {
     const pix = createStaticPix({
-      merchantName: '',
-      merchantCity: '',
+      merchantName: 'Teste',
+      merchantCity: 'Vila Velha',
       pixKey: chavePix.trim(),
       infoAdicional: descricao,
       transactionAmount: valor,
@@ -17,8 +17,8 @@ export class PixService {
       throw new Error('Erro ao gerar o código Pix.');
     }
 
-    const brCode = pix.toBRCode(); // Código para "copia e cola"
-    const qrCodeBase64 = await pix.toImage(); // Imagem QR Code em base64
+    const brCode = pix.toBRCode();
+    const qrCodeBase64 = await pix.toImage();
 
     return {
       brCode,
